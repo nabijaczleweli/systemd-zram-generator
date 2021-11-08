72bfbd5c20c7e70c0c5d9734e7e5e1c98766c0eb
наб <nabijaczleweli@nabijaczleweli.xyz>
2021-11-08 13:30:24 +0100

    squash? reduce to libc

--- tests/test_cases.rs
+++ tests/test_cases.rs
@@ -12,17 +12,31 @@ use tempfile::TempDir;
 
 #[ctor::ctor]
 fn unshorn() {
-    use nix::{mount, sched, unistd};
     use std::os::unix::fs::symlink;
+    use std::ptr;
 
-    let (uid, gid) = (unistd::geteuid(), unistd::getegid());
-    sched::unshare(sched::CloneFlags::CLONE_NEWUSER | sched::CloneFlags::CLONE_NEWNS).unwrap();
+    let (uid, gid) = unsafe { (libc::geteuid(), libc::getegid()) };
+    assert_eq!(
+        unsafe { libc::unshare(libc::CLONE_NEWUSER | libc::CLONE_NEWNS) },
+        0,
+        "Unshare failed. Are unprivileged userns on?"
+    );
     fs::write("/proc/self/setgroups", b"deny").unwrap();
     fs::write("/proc/self/uid_map", format!("0 {} 1", uid)).unwrap();
     fs::write("/proc/self/gid_map", format!("0 {} 1", gid)).unwrap();
 
-    mount::mount::<str, _, _, str>(None, "/proc", Some("tmpfs"), mount::MsFlags::empty(), None)
-        .unwrap();
+    assert_eq!(
+        unsafe {
+            libc::mount(
+                ptr::null(),
+                b"/proc\0".as_ptr() as *const i8,
+                b"tmpfs\0".as_ptr() as *const i8,
+                0,
+                ptr::null(),
+            )
+        },
+        0
+    );
     fs::create_dir("/proc/self").unwrap();
     symlink("zram-generator", "/proc/self/exe").unwrap();
 }
